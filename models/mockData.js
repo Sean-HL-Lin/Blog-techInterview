039be981eb2f1d287bb762f8a8faa24cecf4bd90
Sean-HL-Lin <hongshenglin92@gmail.com>
2019-10-17 22:51:35 +0000

    create mockdata

---
+++ models/mockData.js
@@ -0,0 +1,21 @@
+module.exports = () => {
+
+  data = [
+    {
+      title: "title1",
+      description: "description 1",
+      email: "email 1 "
+    },
+    {
+      title: "title 2",
+      description: "description 2",
+      email: "email 2"
+    },
+    {
+      title: "title 3",
+      description: "description 3",
+      email: "email 1 "
+    },
+  ]
+  return router;
+};
